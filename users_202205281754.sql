63234fc548637c5a83abd23d66bca393e071c614
lcsllima <49926350+lcsllima@users.noreply.github.com>
2022-05-28 17:56:41 -0300

    SQL das tabelas

---
+++ users_202205281754.sql
@@ -0,0 +1,24 @@
+INSERT INTO obra_na_medida.users (name,mail,password) VALUES
+	 ('Lucas Teste','emailteste@testeemail12345.com','$2y$10$MPOg3J3.NRD4iNMUwjaA8uoVAWbMb4AAZ5mEI4u6TNfQ7KPwlohBq'),
+	 ('Lucas Teste','lucas.tec@outlook.com','$2y$10$MPOg3J3.NRD4iNMUwjaA8uoVAWbMb4AAZ5mEI4u6TNfQ7KPwlohBq'),
+	 ('Lucas Teste','emailteste@testeemail123456.com','$2y$10$MPOg3J3.NRD4iNMUwjaA8uoVAWbMb4AAZ5mEI4u6TNfQ7KPwlohBq'),
+	 ('Lucas Teste','emailteste@testeemail1234567.com','$2y$10$MPOg3J3.NRD4iNMUwjaA8uoVAWbMb4AAZ5mEI4u6TNfQ7KPwlohBq'),
+	 ('Lucas Teste','14242421412','$2y$10$MPOg3J3.NRD4iNMUwjaA8uoVAWbMb4AAZ5mEI4u6TNfQ7KPwlohBq'),
+	 ('Lucas Teste','1232','$2y$10$MPOg3J3.NRD4iNMUwjaA8uoVAWbMb4AAZ5mEI4u6TNfQ7KPwlohBq'),
+	 ('Lucas Teste','123','$2y$10$MPOg3J3.NRD4iNMUwjaA8uoVAWbMb4AAZ5mEI4u6TNfQ7KPwlohBq'),
+	 ('Lucas Teste','1234','$2y$10$MPOg3J3.NRD4iNMUwjaA8uoVAWbMb4AAZ5mEI4u6TNfQ7KPwlohBq'),
+	 ('Lucas Teste','lcslima@teste123.com','$2y$10$MPOg3J3.NRD4iNMUwjaA8uoVAWbMb4AAZ5mEI4u6TNfQ7KPwlohBq'),
+	 ('Lucas Teste','4214124124@qaffsafas.com','$2y$10$MPOg3J3.NRD4iNMUwjaA8uoVAWbMb4AAZ5mEI4u6TNfQ7KPwlohBq');
+INSERT INTO obra_na_medida.users (name,mail,password) VALUES
+	 ('Lucas Teste','123@dsa.com','$2y$10$MPOg3J3.NRD4iNMUwjaA8uoVAWbMb4AAZ5mEI4u6TNfQ7KPwlohBq'),
+	 ('Lucas Teste','emailteste@testeemail123495.com','$2y$10$MPOg3J3.NRD4iNMUwjaA8uoVAWbMb4AAZ5mEI4u6TNfQ7KPwlohBq'),
+	 ('Lucas Teste','emailteste@testeemail12341242145.com','$2y$10$MPOg3J3.NRD4iNMUwjaA8uoVAWbMb4AAZ5mEI4u6TNfQ7KPwlohBq'),
+	 ('Lucas Lima','lcslima@teste123456.com','$2y$10$7X9T6WOkKLHOg9njyWoj/us9dHIVW/yN8M6OUi9gy/mSKGOh20OTK'),
+	 ('teste 4','teste2149421@gmail.com','$2y$10$Yx/II7ygkkvAQ7MolyBFOetaPD9h.FMdRUw6Vo9F5i9y8gN51qzCq'),
+	 ('teste 4','testeemail1244124@24142141.com','$2y$10$zuHgGBqPDramb3snte46c.mvZz2dwQ8Zw1mgg3cmQ4Yg6.W55fjb2'),
+	 ('teste 10','teste2144121429421@gmail.com','$2y$10$PyXelfDMseCEaQ71atRzVuUysm3EB7K1chmRPbqlUgOv6OQfcsvvq'),
+	 ('teste 11','emailbom@teste1421.com','$2y$10$XGwMpav6UVGlYez6OL3pluO3hrfIH/z5FXYFE.KxW86inRf1svHYa'),
+	 ('Edinei ','edineiteste@12412409142094.com','$2y$10$MaKCUbq2BR9jSYOJKPw9x.l6uiTGqQYeSWKt5H/Zutdq2zXCx7BCS'),
+	 ('Sabrino','sabrino@obranamedida.com.br','$2y$10$pTsn9BhnG0gwZ.c31Vln/O0oM6Bj4SjrKsqjQ1qyyG4JeMOYoRpXy');
+INSERT INTO obra_na_medida.users (name,mail,password) VALUES
+	 ('Root','teste@123.com','$2y$10$KcC6lXIiZZ7XFnSiACY.yOllCRQFjdyp.Kie5UVnN4EA86zSvG9Ha');
